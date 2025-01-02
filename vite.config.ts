a32bd1dbfcde57e8c8be36aa9a43f6a489a18da4
SulemanAwais <102269316+SulemanAwais@users.noreply.github.com>
2025-01-02 07:24:38 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,13 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: mode === 'production' ? '/stylish-showcase//' : '/', // Replace <repository-name> with your GitHub repo name
   server: {
     host: "::",
     port: 8080,
+  },
+  build: {
+    outDir: "docs", // Output folder for GitHub Pages deployment from main branch
   },
   plugins: [
     react(),
